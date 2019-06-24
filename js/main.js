4cce0616ce334e61d6be66f97ea1667b14aaab46
mindeby <debora.marisa.ribeiro@gmail.com>
2019-06-24 18:21:12 +0200

    redesign

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => { //wait until everything loaded
 
-  
+  let i = 0;
+  let txt = 'and no “h", it’s Portuguese :)';
+  let speed = 60;
+
+  function typeWriter() {
+    if (i <= txt.length) {
+      document.getElementById("typewriting").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  setTimeout(typeWriter,2000);
+
+
+
+
+
 
 });
