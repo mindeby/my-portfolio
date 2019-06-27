d68b6bb0cf61e1f06b59c2042c56448f7fefb366
mindeby <debora.marisa.ribeiro@gmail.com>
2019-06-27 16:36:11 +0200

    submit event

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => { //wait until everything lo
   let hamburger = document.getElementById('hamburger');
   let dropdown = document.getElementById('dropdown');
   let emojis = document.getElementsByClassName('emoji')
+  let form = document.getElementById('my-form');
 
 
   reveal.addEventListener("click", function(e){
@@ -29,7 +30,9 @@ document.addEventListener('DOMContentLoaded', () => { //wait until everything lo
     box.style.transform = "rotateX(90deg)"
   });
 
-
+  form.addEventListener("submit", function(e){
+      document.getElementById('hi').style.display= "none";
+  });
 
   hamburger.addEventListener("click", function(e){
     dropdown.classList.add('expanded');
@@ -43,6 +46,8 @@ document.addEventListener('DOMContentLoaded', () => { //wait until everything lo
 
 
 
+  /*document.getElementById('hi').style.height = '100px';
+  document.getElementById('hi').innerHTML = "<h3>👉 Nice to meet you, I'll get back to you soon!</h3>"*/
 
 
 });
